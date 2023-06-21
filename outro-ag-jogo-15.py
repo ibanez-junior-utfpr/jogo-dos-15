03962a598babb1e43577ae01328e9b98535a7b48
Ibanez Fernandes da Silva Junior - RA:2033500 <ibanezjunior@alunos.utfpr.edu.br>
2023-06-21 00:09:35 -0300

    outro ag

--- outro-ag-jogo-15.py
+++ outro-ag-jogo-15.py
@@ -1,65 +1,71 @@
 import random
+import copy
+import time
 
-def print_board(board):
-    for i in range(4):
-        for j in range(4):
-            print(board[i*4 + j], end="\t")
-        print("\n")
+#Montar Tabela a partir de um vetor
+def monta_tabela(vetor):
+	l1 = [ vetor[0], vetor[1], vetor[2], vetor[3] ]
+	l2 = [ vetor[4], vetor[5], vetor[6], vetor[7] ]
+	l3 = [ vetor[8], vetor[9], vetor[10], vetor[11] ]
+	l4 = [ vetor[12], vetor[13], vetor[14], vetor[15] ]
+	tabela = [ l1, l2, l3, l4 ]
+	return tabela
 
-def get_blank_position(board):
+#mostrar array 4x4 lado a lado
+def mostrar_array(atual, destino):
+	l1 = []
+	l2 = []
+	txt = "{:>5}"
+	for x in atual:
+		det = ""
+		for y in x:
+			det += txt.format(str(y))
+		l1.append(det)
+	for x in destino:
+		det = ""
+		for y in x:
+			det += txt.format(str(y))
+		l2.append(det)
+	print("--------------------\t--------------------")
+	for x in range(0, len(l1)):
+		det = l1[x] + "\t" + l2[x]
+		print(det)
+	print("--------------------\t--------------------")
+
+#Onde está o zero
+def acha_zero(vetor):
     retorno = -1
-    if 0 in board:
-        retorno = board.index(0)
+    if 0 in vetor:
+        retorno = vetor.index(0)
     return retorno
 
-def insert_blank_position(board):
-    board[random.randrange(16)] = 0
-    return board
-
-def get_neighbors(board):
-    neighbors = []
-    blank_pos = get_blank_position(board)
-    if blank_pos >= 4:
-        neighbors.append(swap_tiles(board, blank_pos, blank_pos-4))
-    if blank_pos < 12:
-        neighbors.append(swap_tiles(board, blank_pos, blank_pos+4))
-    if blank_pos % 4 != 0:
-        neighbors.append(swap_tiles(board, blank_pos, blank_pos-1))
-    if blank_pos % 4 != 3:
-        neighbors.append(swap_tiles(board, blank_pos, blank_pos+1))
-    print(neighbors)
-    input("...")
-    return neighbors
+#Função Objetivo: compara_objetivo
+def compara_objetivo(vetor_atual, vetor_objetivo):
+	valor_fit = 0
+	for i in range(len(vetor_objetivo)):
+		a = vetor_atual[i]
+		o = vetor_objetivo[i]
+		valor_fit += abs(a - o) * (i + 1)
+	return valor_fit
 
 def swap_tiles(board, pos1, pos2):
     new_board = board.copy()
     new_board[pos1], new_board[pos2] = new_board[pos2], new_board[pos1]
     return new_board
 
-def manhattan_distance(board):
-    distance = 0
-    for i in range(4):
-        for j in range(4):
-            tile = board[i*4 + j]
-            if tile != 0:
-                target_row = (tile - 1) // 4
-                target_col = (tile - 1) % 4
-                distance += abs(i - target_row) + abs(j - target_col)
-    return distance
-
 def create_population(population_size):
     population = []
-    for _ in range(population_size):
-        individual = list(range(1, 16))
+    while len(population) < population_size:
+        individual = list(range(0, 16))
         random.shuffle(individual)
-        individual.append(0)
-        population.append(individual)
+        if individual not in population:
+            population.append(individual)
     return population
 
-def evaluate_population(population):
+def evaluate_population(population, destino):
     fitness_scores = []
     for individual in population:
-        fitness_scores.append(manhattan_distance(individual))
+        fitness_scores.append(compara_objetivo(individual, destino))
     return fitness_scores
 
 def select_parents(population, fitness_scores):
@@ -77,11 +83,9 @@ def crossover(parent1, parent2):
 def mutate(individual, mutation_rate):
     for i in range(16):
         if random.random() < mutation_rate:
-            blank_pos = get_blank_position(individual)
+            blank_pos = acha_zero(individual)
             if blank_pos >= 0:
                 individual[i], individual[blank_pos] = individual[blank_pos], individual[i]
-            else:
-                individual = insert_blank_position(individual)
     return individual
 
 def evolve_population(population, fitness_scores, mutation_rate):
@@ -94,10 +98,10 @@ def evolve_population(population, fitness_scores, mutation_rate):
         new_population.extend([child1, child2])
     return new_population
 
-def genetic_algorithm(population_size, mutation_rate, max_generations):
+def genetic_algorithm(population_size, mutation_rate, max_generations, destino):
     population = create_population(population_size)
-    for generation in range(max_generations):
-        fitness_scores = evaluate_population(population)
+    for _ in range(max_generations):
+        fitness_scores = evaluate_population(population, destino)
         best_individual = population[fitness_scores.index(min(fitness_scores))]
         if min(fitness_scores) == 0:
             return best_individual
@@ -105,10 +109,12 @@ def genetic_algorithm(population_size, mutation_rate, max_generations):
     return best_individual
 
 # Exemplo de uso
+inicio = time.time()
 population_size = 400
 mutation_rate = 0.1
 max_generations = 200
-
-solution = genetic_algorithm(population_size, mutation_rate, max_generations)
+destino = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 ]
+solution = genetic_algorithm(population_size, mutation_rate, max_generations, destino)
 print("Solução encontrada:")
-print_board(solution)
+mostrar_array(monta_tabela(solution), monta_tabela(destino))
+print(time.time() - inicio, "Tempo decorrido (Final)")
